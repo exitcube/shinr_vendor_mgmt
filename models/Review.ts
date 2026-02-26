c38207858cf34c406cdfec5427941710163276e0
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2026-02-26 14:48:14 +0530

    VendorRelated-table

--- models/Review.ts
+++ models/Review.ts
@@ -1,36 +1,36 @@
 
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm";
-import { Vendor } from "./Vendor";
+// import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm";
+// import { Vendor } from "./Vendor";
 
-@Entity()
-export class Review {
-    @PrimaryGeneratedColumn()
-    id: number;
+// @Entity()
+// export class Review {
+//     @PrimaryGeneratedColumn()
+//     id: number;
 
-    @ManyToOne(() => Vendor, { onDelete: "CASCADE" })
-    vendor: Vendor;
+//     @ManyToOne(() => Vendor, { onDelete: "CASCADE" })
+//     vendor: Vendor;
 
-    @Column()
-    vendorId: number;
+//     @Column()
+//     vendorId: number;
 
-    @Column()
-    customerId: string;
+//     @Column()
+//     customerId: string;
 
-    @Column({ type: "int" })
-    rating: number; // 1-5
+//     @Column({ type: "int" })
+//     rating: number; // 1-5
 
-    @Column("text", { nullable: true })
-    comment: string;
+//     @Column("text", { nullable: true })
+//     comment: string;
 
-    @Column("text", { nullable: true })
-    reply: string;
+//     @Column("text", { nullable: true })
+//     reply: string;
 
-    @Column({ default: false })
-    isFlagged: boolean;
+//     @Column({ default: false })
+//     isFlagged: boolean;
 
-    @CreateDateColumn()
-    createdAt: Date;
+//     @CreateDateColumn()
+//     createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
-}
+//     @UpdateDateColumn()
+//     updatedAt: Date;
+// }
--- models/index.ts
+++ models/index.ts
@@ -1,11 +1,28 @@
-
 import { Vendor } from "./Vendor";
-import { Review } from "./Review";
 import { VendorFile } from "./VendorFile";
 import { File } from "./File";
+import { VendorOrganization } from "./VendorOrganization";
+import { VendorOrganizationToken } from "./VendorOrganizationToken";
+import { VendorToken } from "./VendorToken";
+// import { Product } from './Product';
 
 
+// Export all entities as an array for TypeORM configuration
+export const entities = [
+    Vendor,
+    VendorFile,
+    File,
+    VendorOrganization,
+    VendorOrganizationToken,
+    VendorToken
+    // Product,
+];
 
 
-export { Vendor,VendorFile,File, Review };
-export const entities = [Vendor, VendorFile,File, Review];
+// Export individual entities and types
+export { Vendor, type Vendor as VendorType } from './Vendor';
+export { VendorFile, type VendorFile as VendorFileType } from './VendorFile';
+export { File, type File as FileType } from "./File";
+export { VendorOrganization, type VendorOrganization as VendorOrganizationType,} from "./VendorOrganization";
+export { VendorOrganizationToken, type VendorOrganizationToken as VendorOrganizationTokenType,} from "./VendorOrganizationToken";
+export { VendorToken, type VendorToken as VendorTokenType } from "./VendorToken";
