bbf3fb772b089fba2b69e6972d257e4707c0deb0
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2026-02-26 10:15:26 +0530

    updtesssssss

--- models/index.ts
+++ models/index.ts
@@ -1,9 +1,11 @@
 
 import { Vendor } from "./Vendor";
 import { Review } from "./Review";
+import { VendorFile } from "./VendorFile";
+import { File } from "./File";
 
 
 
 
-export { Vendor, Review };
-export const entities = [Vendor, Review];
+export { Vendor,VendorFile,File, Review };
+export const entities = [Vendor, VendorFile,File, Review];
